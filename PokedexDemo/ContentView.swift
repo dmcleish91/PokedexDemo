a49f33f723ba518fb2b9624de490be08176d8eec
Dwight McLeish Jr <dmcleishjr@gmail.com>
2025-08-07 21:36:07 -0400

    Initial Commit

---
+++ PokedexDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PokedexDemo
+//
+//  Created by Dwight Mcleish Jr on 8/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PokedexDemo/PokedexDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexDemoApp.swift
+//  PokedexDemo
+//
+//  Created by Dwight Mcleish Jr on 8/7/25.
+//
+
+import SwiftUI
+
+@main
+struct PokedexDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
